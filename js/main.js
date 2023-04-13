cb1e737e2ecfb5060a754156046f2f664b10eb8e
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-13 16:18:09 +0200

    stampa su file HTML

--- js/main.js
+++ js/main.js
@@ -1,26 +1,35 @@
 //Chiedere all'utente quanti kilometri vuole fare
 const kilometri = Number(prompt("Quanti Kilometri vuoi fare?"));
-console.log(`L'utente deve percorrere ${kilometri}`);
 
 //Chiedere all'utente quanti anni ha
 const eta = Number(prompt("Quanti anni hai?"));
-console.log(`L'utente dichiara di avere ${eta} anni`);
 
 //Prezzo BIglietto
 let prezzo = 0.21; //prezzo di ogni kilometro
 
 let prezzoKm = (kilometri * prezzo).toFixed(2);
-console.log(`Il prezzo dell'biglietto è ${prezzoKm}€`)
 
 let prezzoTotale;
+
+//Stampa sul file HTML i kilometri dall'utente Inserito
+let KilometriDaFare = (`I kilometri che vuoi percorrere sono : ${kilometri} km`);
+document.getElementById("kilometri").innerText = KilometriDaFare;
+
+//Stampa sul file HTML gli anni dall'utente inserito
+let anniUtente = (`Gli anni dall'utente inserito : ${eta} anni`);
+document.getElementById("eta").innerText = anniUtente;
 
 //controllo per lo sconto dell'eta'
 if (eta < 18) {
     prezzoTotale = (prezzoKm * 0.2).toFixed(2);
-    console.log(`Il prezzo dell'biglietto è ${prezzoTotale}€`)
+    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale}`);
+    document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 } else if (eta > 65) {
     prezzoTotale = (prezzoKm * 0.4).toFixed(2);
-    console.log(`Il prezzo dell'biglietto è ${prezzoTotale}€`)
+    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale}`);
+    document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 } else {
     prezzoTotale = prezzoKm;
+    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale}`);
+    document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 }

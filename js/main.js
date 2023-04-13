228f244a0177d755dab3ba616d323b7ed7cbab46
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-13 16:33:47 +0200

    if for NaN

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const kilometri = Number(prompt("Quanti Kilometri vuoi fare?"));
 //Chiedere all'utente quanti anni ha
 const eta = Number(prompt("Quanti anni hai?"));
 
+
+if (kilometri == NaN) {
+    alert(`si prega si inserire nuovamnete i dati corretti`)
+}
+
 //Prezzo BIglietto
 let prezzo = 0.21; //prezzo di ogni kilometro
 
@@ -22,14 +27,14 @@ document.getElementById("eta").innerText = anniUtente;
 //controllo per lo sconto dell'eta'
 if (eta < 18) {
     prezzoTotale = (prezzoKm * 0.2).toFixed(2);
-    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale}`);
+    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale} €`);
     document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 } else if (eta > 65) {
     prezzoTotale = (prezzoKm * 0.4).toFixed(2);
-    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale}`);
+    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale} €`);
     document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 } else {
     prezzoTotale = prezzoKm;
-    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale}`);
+    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale} €`);
     document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 }

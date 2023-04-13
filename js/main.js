42c74c008725bb6fe9e3549c4da33ae5c43cad36
MathiasChirivi <chirivimathias3@gmail.com>
2023-04-13 17:05:51 +0200

    output alert and pageHTML

--- js/main.js
+++ js/main.js
@@ -6,9 +6,11 @@ const eta = Number(prompt("Quanti anni hai?"));
 
 
 if (isNaN(kilometri)) {
-    alert(`si prega si inserire nuovamnete i dati corretti`);
+    alert(`si prega si inserire nuovamente i dati corretti`);
 } else if (isNaN(eta)) {
-    alert(`si prega si inserire nuovamnete i dati corretti`);
+    alert(`si prega si inserire nuovamente i dati corretti`);
+} else {
+    alert(`I dati sono stati inseriti corretamente`)
 }
 
 //Prezzo BIglietto
@@ -29,14 +31,17 @@ document.getElementById("eta").innerText = anniUtente;
 //controllo per lo sconto dell'eta'
 if (eta < 18) {
     prezzoTotale = (prezzoKm * 0.2).toFixed(2);
-    let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale} €`);
-    document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
+   alert(`Il prezzo del biglietto è : ${prezzoTotale} €, ed usufluisci del 20% di sconto avendo meno di 18 anni!`);
+   let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale} €`);
+   document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 } else if (eta > 65) {
     prezzoTotale = (prezzoKm * 0.4).toFixed(2);
+    alert(`Il prezzo del biglietto è : ${prezzoTotale} €, ed usufluisci del 40% di sconto avendo piu di 65 anni!`);
     let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale} €`);
     document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 } else {
     prezzoTotale = prezzoKm;
+    alert(`Il prezzo del biglietto è : ${prezzoTotale} €`);
     let prezzoTotaleTicket = (`Il prezzo del biglietto è : ${prezzoTotale} €`);
     document.getElementById("prezzoTotale").innerText = prezzoTotaleTicket;
 }
